c1c1082bbde761901ce9f823db7de6bcaf6eb979
Ubuntu <ubuntu@ip-172-31-29-79.ap-south-1.compute.internal>
2020-08-18 18:24:40 +0000

    Add New Project webcicd app

---
+++ src/test/java/com/simplilearn/webcicddemo/WebcicddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.webcicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcicddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
